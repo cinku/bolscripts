3a97e5e7a8633d96bfd35849bf21979426fb77dd
cinkulol <mmarczakm@gmail.com>
2015-09-21 19:38:16 +0200

    ashefixasd23

--- NeXtGenAshe.lua
+++ NeXtGenAshe.lua
@@ -1,14 +1,37 @@
 if myHero.charName ~= "Ashe" then return end
 
+local version = "1.2"
+local AUTOUPDATE = true
+local UPDATE_HOST = "raw.github.com"
+local UPDATE_PATH = "/cinkulol/bolscripts/master/NeXtGenAshe.lua".."?rand="..math.random(1,10000)
+local UPDATE_FILE_PATH = LIB_PATH.."NeXtGenAshe.lua"
+local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
+
+function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>SAC VI:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
+if AUTOUPDATE then
+	local ServerData = GetWebResult(UPDATE_HOST, UPDATE_PATH)
+	if ServerData then
+		local ServerVersion = string.match(ServerData, "local version = \"%d+.%d+\"")
+		ServerVersion = string.match(ServerVersion and ServerVersion or "", "%d+.%d+")
+		if ServerVersion then
+			ServerVersion = tonumber(ServerVersion)
+			if tonumber(version) < ServerVersion then
+				AutoupdaterMsg("New version available"..ServerVersion)
+				AutoupdaterMsg("Updating, please don't press F9")
+				DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end)	 
+			else
+				AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
+			end
+		end
+	else
+		AutoupdaterMsg("Error downloading version info")
+	end
+end
+
 require 'SxOrbWalk'
 require 'VPrediction'
 
 --local variables
-local version = "1.2"
-local SCRIPT_NAME = "NeXtGenAshe"
-local SOURCELIB_URL = "https://raw.github.com/TheRealSource/public/master/common/SourceLib.lua"
-local SOURCELIB_PATH = LIB_PATH.."SourceLib.lua"
-local AUTOUPDATE = true
 local enemies = {}
 local minions = {}
 local jungleMinions = {}
@@ -19,19 +42,6 @@ local spells =
 	E = { ready = false, mana = 0, range = 2500, delay = 0.25, width = 299, speed = 1400},
 	R = { ready = false, mana = 0, range = 3000, delay = 0.25, width = 130, speed = 1600}
 }
-
-if FileExist(SOURCELIB_PATH) then
-	require("SourceLib")
-else
-    DOWNLOADING_SOURCELIB = true
-    DownloadFile(SOURCELIB_URL, SOURCELIB_PATH, function() print("Required libraries downloaded successfully, please reload") end)
-end
-
-if DOWNLOADING_SOURCELIB then print("Downloading required libraries, please wait...") return end
-
-if AUTOUPDATE then
-     SourceUpdater(SCRIPT_NAME, version, "raw.github.com", "/cinkulol/bolscripts/master/"..SCRIPT_NAME..".lua", SCRIPT_PATH .. GetCurrentEnv().FILE_NAME, "/cinkulol/bolscripts/master/"..SCRIPT_NAME..".version"):CheckUpdate()
-end
 
 --hooks
 
