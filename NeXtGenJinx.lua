021dc329a3878853b46dfb0c568cfef78f457020
cinkulol <mmarczakm@gmail.com>
2015-09-22 16:33:23 +0200

    fix2

--- NeXtGenJinx.lua
+++ NeXtGenJinx.lua
@@ -3,7 +3,6 @@ if myHero.charName ~= "Jinx" then return end
 require 'VPrediction'
 
 --local variables
-local currentVersion = 1.2
 local enemies = {}
 local minions = {}
 local jungleMinions = {}
@@ -35,7 +34,6 @@ local Spells =
 
 function OnLoad()
 	loadOrbwalker()
-	checkUpdate()
 	variables()
 	menu()
 end
@@ -564,115 +562,4 @@ function OnDraw()
 			DrawLine3D(myHero.x, myHero.y, myHero.z, target.x, target.y, target.z, 3, RGB(255,0,0))
 		end
 	end
-end
-
-class 'AutoUpdate'
-
-function AutoUpdate:__init(localVersion, host, versionPath, scriptPath, savePath, callbackUpdate, callbackNoUpdate, callbackNewVersion, callbackError)
-	self.localVersion = localVersion
-	self.versionPath = host .. versionPath
-	self.scriptPath = host .. scriptPath
-	self.savePath = savePath
-	self.callbackUpdate = callbackUpdate
-	self.callbackNoUpdate = callbackNoUpdate
-	self.callbackNewVersion = callbackNewVersion
-	self.callbackError = callbackError
-	self:createSocket(self.versionPath)
-	self.downloadStatus = 'Connect to Server for VersionInfo'
-	AddTickCallback(function() self:getVersion() end)
-end
-
-function AutoUpdate:createSocket(url)
-	if not self.LuaSocket then
-	    self.LuaSocket = require("socket")
-	else
-	    self.socket:close()
-	    self.socket = nil
-	end
-	self.LuaSocket = require("socket")
-	self.socket = self.LuaSocket.tcp()
-	self.socket:settimeout(0, 'b')
-	self.socket:settimeout(99999999, 't')
-	self.socket:connect("linkpad.fr", 80)
-	self.url = "/aurora/TcpUpdater/getscript.php?page=" .. url
-	self.started = false
-	self.File = ''
-end
-
-function AutoUpdate:getVersion()
-	if self.gotScriptVersion then return end
-
-	local Receive, Status, Snipped = self.socket:receive(1024)
-	if Status == 'timeout' and self.started == false then
-		self.started = true
-	    self.socket:send("GET ".. self.url .." HTTP/1.0\r\nHost: linkpad.fr\r\n\r\n")
-	end
-	if (Receive or (#Snipped > 0)) then
-		self.File = self.File .. (Receive or Snipped)
-	end
-	if Status == "closed" then
-		local _, ContentStart = self.File:find('<script'..'data>')
-		local ContentEnd, _ = self.File:find('</script'..'data>')
-		if not ContentStart or not ContentEnd then
-		    self.callbackError()
-		else
-			self.onlineVersion = tonumber(self.File:sub(ContentStart + 1,ContentEnd-1))
-			if self.onlineVersion > self.localVersion then
-				self.callbackNewVersion(self.onlineVersion)
-				self:createSocket(self.scriptPath)
-				self.DownloadStatus = 'Connect to Server for ScriptDownload'
-				AddTickCallback(function() self:downloadUpdate() end)
-
-			elseif self.onlineVersion <= self.localVersion then
-				self.callbackNoUpdate()
-			end
-		end
-		self.gotScriptVersion = true
-
-	end
-end
-
-function AutoUpdate:downloadUpdate()
-	if self.gotScriptUpdate then return end
-
-	local Receive, Status, Snipped = self.socket:receive(1024)
-	if Status == 'timeout' and self.started == false then
-		self.started = true
-	    self.socket:send("GET ".. self.url .." HTTP/1.0\r\nHost: linkpad.fr\r\n\r\n")
-	end
-	if (Receive or (#Snipped > 0)) then
-		self.File = self.File .. (Receive or Snipped)
-	end
-	if Status == "closed" then
-		local _, ContentStart = self.File:find('<script'..'data>')
-		local ContentEnd, _ = self.File:find('</script'..'data>')
-
-		if not ContentStart or not ContentEnd then
-		    self.callbackError()
-		else
-			self.File = self.File:sub(ContentStart + 1,ContentEnd-1)
-			local f = io.open(self.savePath,"w+b")
-			f:write(self.File)
-			f:close()
-			self.callbackUpdate(self.onlineVersion, self.localVersion)
-		end
-		self.gotScriptUpdate = true
-
-	end
-end
-
-local fileLoc = SCRIPT_PATH.."/" .. GetCurrentEnv().FILE_NAME
-function checkUpdate()
-	local ToUpdate = {}
-	ToUpdate.Version = currentVersion
-	ToUpdate.Name = "NeXtGenJinx"
-	ToUpdate.Host = "raw.githubusercontent.com"
-	ToUpdate.VersionPath = "/cinkulol/bolscripts/master/versions/NeXtGenJinx.version"
-	ToUpdate.ScriptPath =  "/cinkulol/bolscripts/master/NeXtGenJinx.lua"
-	ToUpdate.SavePath = fileLoc
-	ToUpdate.CallbackUpdate = function(NewVersion,OldVersion) print("<font color=\"#0084FF\"><b>[" .. ToUpdate.Name .. "] AutoUpdater - </b></font> <font color=\"#FFFFFF\">Updated to "..NewVersion..". Please Reload with 2x F9</b></font>") end
-	ToUpdate.CallbackNoUpdate = function() print("<font color=\"#0084FF\"><b>[" .. ToUpdate.Name .. "] AutoUpdater - </b></font> <font color=\"#FFFFFF\">No Updates Found</b></font>") end
-	ToUpdate.CallbackNewVersion = function(NewVersion) print("<font color=\"#0084FF\"><b>[" .. ToUpdate.Name .. "] AutoUpdater - </b></font> <font color=\"#FFFFFF\">New Version found ("..NewVersion.."). Please wait until its downloaded</b></font>") end
-	ToUpdate.CallbackError = function(NewVersion) print("<font color=\"#0084FF\"><b>[" .. ToUpdate.Name .. "] AutoUpdater - </b></font> <font color=\"#FFFFFF\">Error while Downloading. Please try again.</b></font>") end
-	AutoUpdate(ToUpdate.Version, ToUpdate.Host, ToUpdate.VersionPath, ToUpdate.ScriptPath, ToUpdate.SavePath, ToUpdate.CallbackUpdate,ToUpdate.CallbackNoUpdate, ToUpdate.CallbackNewVersion,ToUpdate.CallbackError)
 end
